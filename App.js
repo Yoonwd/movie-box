9e0587ef48108d4cb02f0177c30b941eea19795e
Yoon <yoonwdwd@gmail.com>
2020-04-13 06:21:10 +0900

    * add App setting
    - add loading

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading, Asset } from 'expo';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  const [loaded, setLoaded] = useState(false);
+
+  const handleError = (error) => console.log(error);
+
+  const handleLoaded = () => setLoaded(true);
+
+  const loadAssets = async() => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+    // await Asset.loadAsync([
+    //   require("images/icon.png")
+    // ])
+  }
+  
+  if (loaded) {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={loadAssets} 
+        onFinish={handleLoaded} 
+        onError={handleError}
+      />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
